ea4cc5a3fb3734f41dd813474cbfe7c5dd429029
Arpita K Sau <arpitakaran@gmail.com>
2020-08-08 17:37:37 -0500

    fixed-connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,14 @@
+
 const mysql = require("mysql");
-
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    let connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "rootarpita1",
-        database: "burgers_db"
-    });
-};
-
-connection.connect(err => err ? console.error(`Error connecting to DB: ${err.stack}`) : console.log(`Connected as ID: ${connection.threadId}`));
-
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "rootarpita1",
+    database: "burgers_db"
+  });
+}
+connection.connect();
 module.exports = connection;
