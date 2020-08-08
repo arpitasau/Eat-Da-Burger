d8ee9c5a43d74379a2eb346ff8d1e3fb359b107f
Arpita K Sau <arpitakaran@gmail.com>
2020-08-08 13:07:47 -0500

    added connection and main handlebars

---
+++ config/orm.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql");
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    let connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "rootarpita1",
+        database: "burgers_db"
+    });
+};
+
+connection.connect(err => err ? console.error(`Error connecting to DB: ${err.stack}`) : console.log(`Connected as ID: ${connection.threadId}`));
+
+module.exports = connection;
