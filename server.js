864f92e37702ac9b716c0c2f4d3c3d657bb6cffb
Arpita K Sau <arpitakaran@gmail.com>
2020-08-08 13:43:27 -0500

    added index.handlebars

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-const connection = require("connection");
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, function(){
+console.log(`server listening on: ${PORT}`);
+});
